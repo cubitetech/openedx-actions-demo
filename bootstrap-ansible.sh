23498e90528b23c216cc9b72bc0a14861021e739
Cubite <devops@cubite.io>
2021-11-16 19:57:08 +0000

    feat: Deploy Open edX vars using Github actions

---
+++ bootstrap-ansible.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -r /var/lib/apt/lists/* -vf
+apt-get update
+apt-get install -y sudo git python3-pip python2.7 python2.7-dev python-apt python-jinja2 build-essential sudo git-core libmysqlclient-dev libffi-dev libssl-dev python3-pip python3-apt python3-jinja2 build-essential sudo git-core libmysqlclient-dev libffi-dev libssl-dev
+git clone https://github.com/edx/configuration
+cd configuration/
+git checkout open-release/lilac.master
+make requirements
+cd playbooks/
+ansible-playbook edx_ansible.yml -i '127.0.0.1,' -c local -e "CONFIGURATION_VERSION=open-release/lilac.master"
+chmod 400 ~/ssh_key
