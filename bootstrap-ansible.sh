8cdc43ef0294fbc778eb39c394f1230fe2508705
Cubite <devops@cubite.io>
2021-11-18 07:20:13 +0000

    add vault password to the playbook command

--- bootstrap-ansible.sh
+++ bootstrap-ansible.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 sudo rm -r /var/lib/apt/lists/* -vf
-sudo apt-get update
-sudo apt-get install -y sudo git python3-pip python2.7 python2.7-dev python-apt python-jinja2 build-essential sudo git-core libmysqlclient-dev libffi-dev libssl-dev python3-pip python3-apt python3-jinja2 build-essential sudo git-core libmysqlclient-dev libffi-dev libssl-dev
-git clone https://github.com/edx/configuration
-cd configuration/
-git checkout open-release/lilac.master
-make requirements
-cd playbooks/
-ansible-playbook edx_ansible.yml -i '127.0.0.1,' -c local -e "CONFIGURATION_VERSION=open-release/lilac.master"
+sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
+wget https://raw.githubusercontent.com/cubiteDevops/do-edx-configs/main/ansible-bootstrap.sh -O - | sudo bash
